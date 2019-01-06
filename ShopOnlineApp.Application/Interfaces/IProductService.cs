e85724f70ce6300cec1ed0d39fdb595707aeb2ab
danghung <danghonghung31121995@gmail.com>
2019-01-06 19:55:06 +0700

    implement function user interface

--- ShopOnlineApp.Application/Interfaces/IProductService.cs
+++ ShopOnlineApp.Application/Interfaces/IProductService.cs
@@ -20,10 +20,8 @@ namespace ShopOnlineApp.Application.Interfaces
         void AddQuantity(int productId, List<ProductQuantityViewModel> quantities);
         List<ProductQuantityViewModel> GetQuantities(int productId);
         void AddImages(int productId, string[] images);
-
         List<ProductImageViewModel> GetImages(int productId);
         void AddWholePrice(int productId, List<WholePriceViewModel> wholePrices);
-
         List<WholePriceViewModel> GetWholePrices(int productId);
     }
 }
